29d75c077a043526cfa12f4de7b5b551b58cac6e
Jude <judetacwel007@gmail.com>
2022-11-24 23:37:08 -0500

    End to End testing of purchasing mobile phones

---
+++ MobliePrac.java
@@ -0,0 +1,65 @@
+package GK;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class MobliePrac {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\judet\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		//driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); // implicit wait
+		driver.get("https://rahulshettyacademy.com/loginpagePractise/");
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.cssSelector("input[id='username']")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.cssSelector("input[id='password']")).sendKeys("learning");
+		driver.findElement(By.cssSelector(".customradio:nth-child(2)")).click();
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(7000));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("okayBtn")));
+		driver.findElement(By.id("okayBtn")).click();
+		
+		WebElement option=  driver.findElement(By.cssSelector("select[class='form-control']"));
+		Select dropdown = new Select(option);
+		dropdown.selectByValue("consult");
+		driver.findElement(By.id("terms")).click();
+		driver.findElement(By.id("signInBtn")).click();
+		
+		
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.partialLinkText("Checkout")));
+		
+		List <WebElement> products = driver.findElements(By.cssSelector(".card-footer .btn-info"));	
+		
+		for(int i =0;i<products.size();i++)
+
+		{
+
+		products.get(i).click();
+
+		}
+
+		driver.findElement(By.partialLinkText("Checkout")).click();}
+
+
+
+
+
+
+
+	}
+
+
